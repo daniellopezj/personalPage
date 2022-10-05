316ce93d1f1b6c48643a7a6305cc0f4253253079
Daniel Lopez <daniellopezj0327@gmail.com>
2022-10-05 09:58:54 -0500

    completed main-view

--- src/app/modules/main/components/main-view/main-view.component.ts
+++ src/app/modules/main/components/main-view/main-view.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faLinkedinIn, faGithub, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-main-view',
@@ -6,6 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./main-view.component.scss']
 })
 export class MainViewComponent  {
+
+  public controls = [
+    {
+      icon: faLinkedinIn,
+      name: 'linkedIn',
+      link: 'https://www.linkedin.com/in/daniel-lopezj/'
+    },
+    {
+      icon: faTwitter,
+      name: 'twitter',
+      link: 'https://twitter.com/daniellopezj01'
+    },
+    {
+      icon: faGithub,
+      name: 'hithub',
+      link: 'https://github.com/dlopez-dev'
+    },
+    {
+      icon: faInstagram,
+      name: 'instagram',
+      link: 'https://www.instagram.com/daniellopezzj/'
+    },
+
+  ]
 
   constructor() { }
 
