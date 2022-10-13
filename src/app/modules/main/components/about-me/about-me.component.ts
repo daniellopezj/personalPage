99d53d9ce2fe2e862f1952b5467eb942612f42f3
Daniel Lopez <daniellopezj0327@gmail.com>
2022-10-13 07:57:01 -0500

    create footer

--- src/app/modules/main/components/about-me/about-me.component.ts
+++ src/app/modules/main/components/about-me/about-me.component.ts
@@ -8,7 +8,6 @@ import { Component } from '@angular/core';
 export class AboutMeComponent {
 
   constructor() { }
-  public technicalSkills = ['Angular', 'React', 'Vue', 'Cypress', 'Jest', 'Git', 'Node js', 'Express', 'AWS', 'Mongo db', 'Redux', 'Rxjs', 'Html/css', 'Responsive design', "SKILLS.ITEM2.HERRAMIENTA1"]
-  // public humanSkills =['SKILLS.ITEM3.HERRAMIENTA1','SKILLS.ITEM3.HERRAMIENTA2','SKILLS.ITEM3.HERRAMIENTA3','SKILLS.ITEM3.HERRAMIENTA4','SKILLS.ITEM3.HERRAMIENTA5','SKILLS.ITEM3.HERRAMIENTA6','SKILLS.ITEM3.HERRAMIENTA7','SKILLS.ITEM3.HERRAMIENTA8','SKILLS.ITEM3.HERRAMIENTA9',]
-  public humanSkills =['SKILLS.ITEM3.HERRAMIENTA1','SKILLS.ITEM3.HERRAMIENTA3','SKILLS.ITEM3.HERRAMIENTA4','SKILLS.ITEM3.HERRAMIENTA5','SKILLS.ITEM3.HERRAMIENTA6','SKILLS.ITEM3.HERRAMIENTA7','SKILLS.ITEM3.HERRAMIENTA8','SKILLS.ITEM3.HERRAMIENTA9',]
+  public technicalSkills = ['Angular', 'React', 'Vue','TypeScript','JavaScript', 'Cypress', 'Jest', 'Git', 'Node js', 'Express', 'AWS', 'Mongo db', 'Redux', 'Rxjs', 'Html/css', 'Responsive design']
+  // public humanSkills =['SKILLS.ITEM3.HERRAMIENTA1','SKILLS.ITEM3.HERRAMIENTA3','SKILLS.ITEM3.HERRAMIENTA4','SKILLS.ITEM3.HERRAMIENTA5','SKILLS.ITEM3.HERRAMIENTA6','SKILLS.ITEM3.HERRAMIENTA7','SKILLS.ITEM3.HERRAMIENTA8','SKILLS.ITEM3.HERRAMIENTA9',]
 }
